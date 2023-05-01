ac886e8a17e8e6d2b60846bcf1e99a42cf3061ff
Xcaliburns <davidabruzzogames@gmail.com>
2023-05-01 16:39:50 +0200

    cleaned project

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -5,12 +5,8 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
     public GameObject player;
-    public Vector3 offset = new Vector3(0, 5, -7);
+    private Vector3 offset = new Vector3(0, 4.4f, -11);
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void LateUpdate()
