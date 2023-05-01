7b3d232db170b882cf42f3e6690dc5ce5d4f427a
Xcaliburns <davidabruzzogames@gmail.com>
2023-05-01 17:00:37 +0200

    challenge01 completed

--- Assets/Challenge 1/Scripts/FollowPlayerX.cs
+++ Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -5,16 +5,13 @@ using UnityEngine;
 public class FollowPlayerX : MonoBehaviour
 {
     public GameObject plane;
-    private Vector3 offset;
+    public Vector3 offset= new Vector3(0, 2.0f, -11);
 
     // Start is called before the first frame update
-    void Start()
-    {
-
-    }
+   
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
         transform.position = plane.transform.position + offset;
     }
